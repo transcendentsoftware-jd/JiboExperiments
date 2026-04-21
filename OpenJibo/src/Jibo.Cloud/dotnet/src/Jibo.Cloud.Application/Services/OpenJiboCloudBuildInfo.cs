c9916aa39567ef15da80bb9867fc13d0c5147ce6
Jacob Dubin <jacob.dubin@transcendentsoftware.com>
2026-04-20 23:16:27 -0500

    version words

--- OpenJibo/src/Jibo.Cloud/dotnet/src/Jibo.Cloud.Application/Services/OpenJiboCloudBuildInfo.cs
+++ OpenJibo/src/Jibo.Cloud/dotnet/src/Jibo.Cloud.Application/Services/OpenJiboCloudBuildInfo.cs
@@ -2,7 +2,9 @@ namespace Jibo.Cloud.Application.Services;
 
 public static class OpenJiboCloudBuildInfo
 {
-    public const string Version = "1.0.10";
+    public const string Version = "1.0.11";
 
-    public static string SpokenVersion => $"Open Jibo Cloud version {Version}.";
+    public static string VersionWords => Version.Replace(".", " dot ");
+
+    public static string SpokenVersion => $"Open Jibo Cloud version {VersionWords}.";
 }
