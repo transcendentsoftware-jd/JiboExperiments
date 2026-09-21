bb39234389fa1de69afed599d538bbc3347260b6
Jacob Dubin <jacob.dubin@transcendentsoftware.com>
2026-09-21 00:20:13 -0500

    Fix credential continuity test import

--- OpenJibo/tests/Jibo.Cloud.Tests/Protocol/AwsSigV4RequestVerifierTests.cs
+++ OpenJibo/tests/Jibo.Cloud.Tests/Protocol/AwsSigV4RequestVerifierTests.cs
@@ -1,6 +1,7 @@
 using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using Jibo.Cloud.Application.Abstractions;
 using Jibo.Cloud.Application.Services;
 using Jibo.Cloud.Domain.Models;
